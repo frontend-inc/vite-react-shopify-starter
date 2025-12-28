7baae42d13bb742ab6632ed9f21425cbb79372be
Rami Bitar <rbitar@gmail.com>
2025-12-28 08:52:08 -0500

    Update to use browser router

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-6xl font-bold">Start prompting</h1>
+      <h1 className="text-6xl font-bold font-heading">Start Prompting</h1>
     </div>
   );
 };
