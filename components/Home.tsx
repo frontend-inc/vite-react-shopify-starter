efc94790c5a10a38d15d48982dac0498cb219583
Rami Bitar <rbitar@gmail.com>
2025-12-29 22:02:44 -0500

    fix cart design

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Products from './Products';
 
 const Home: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <div className="flex items-center justify-center py-16">
-          <h1 className="text-6xl font-bold font-heading">Start Prompting</h1>
-        </div>
-        <Products />
-      </main>
-      <Footer />
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-6xl font-bold font-heading">Start Prompting</h1>
     </div>
   );
 };
